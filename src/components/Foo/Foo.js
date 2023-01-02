4624bb4551df05815ad99afceb716e9cf9ba52a5
grace <1027502235@qq.com>
2023-01-02 19:58:12 +0800

    feat(plop): plop的基本使用

---
+++ src/components/Foo/Foo.js
@@ -0,0 +1,5 @@
+import React from 'react'; export default () => (
+<div className="Foo">
+  <h1>Foo Component</h1>
+</div>
+)
---
+++ src/components/Header/Header.js
@@ -0,0 +1,6 @@
+import React from "react"
+export default () => (
+  <div className="Header">
+    <h1>Header Component</h1>
+  </div>
+)
